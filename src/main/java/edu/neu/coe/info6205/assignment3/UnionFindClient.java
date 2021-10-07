626485576902c970d40a478c1437d7782b6d52dc
Varun Venkatesh Gowda <gowda.v@northeastern.edu>
2021-10-06 22:30:48 -0400

    Implemented UnionFindClient

---
+++ src/main/java/edu/neu/coe/info6205/assignment3/UnionFindClient.java
@@ -0,0 +1,28 @@
+package edu.neu.coe.info6205.assignment3;
+
+import edu.neu.coe.info6205.union_find.UF_HWQUPC;
+
+import java.util.Random;
+
+public class UnionFindClient {
+    public static void main (String[] args){
+        int n = 5000;
+        int connections = count(n);
+        System.out.println("Connections are "+connections);
+    }
+
+    public static int count (int n){
+        Random random = new Random();
+        UF_HWQUPC uf= new UF_HWQUPC(n, true);
+        int pairsGenerated = 0;
+        while (uf.components()!=1){
+            int s1 = random.nextInt(n);
+            int s2 = random.nextInt(n);
+            pairsGenerated++;
+            if(!uf.connected(s1, s2)){
+                uf.union(s1,s2);
+            }
+        }
+        return pairsGenerated;
+    }
+}
